f8b818b5a8609d7f0dc5c6b34adbb3645db18ac0
Quốc Hùng <Quochung1st-dev@users.noreply.github.com>
2024-04-22 08:13:57 +0700

    code

---
+++ src/components/Header.tsx
@@ -0,0 +1,24 @@
+const Header = () => (
+  <div className="app__header">
+    <h1 className="app__title">snippet-generator</h1>
+    <p className="app__subtitle">
+      Made by{" "}
+      <a className="app__link" href="https://vnhte.com/">
+        Quốc Hùng Dev
+      </a>{" "}
+      |{" "}
+      <a className="app__link" href="https://vnhte.com/donate-ung-ho-duy-tri-va-phat-trien-website/">
+        Buy me a coffee
+      </a>{" "}
+      | Source code on{" "}
+      <a
+        className="app__link"
+        href="https://github.com/Quochung1st-dev"
+      >
+        GitHub
+      </a>
+    </p>
+  </div>
+);
+
+export default Header;
